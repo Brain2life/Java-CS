8867d6dca361733504c71729cf64977bbacbf935
brain2life <maxat.akbanov@gmail.com>
2020-08-27 23:14:38 +0600

    init new repository and add first exercise

---
+++ README.md
@@ -0,0 +1,5 @@
+## Programming exercises solved from the book "Computer Science An Interdisciplinary Approach
+
+This repository represents programming exercises solved from Robert Sedgewick's and Kevin Wayne's [book](https://www.amazon.com/Introduction-Programming-Java-Interdisciplinary-Approach/dp/0321498054)
+
+According exercise numbers can be found at the comment section of the files. 
---
+++ chapter1.2/CheckTriangleSides.java
@@ -0,0 +1,16 @@
+// Exercise 1.2.15
+// javac CheckTriangleSides
+// java CheckTriangle Sides [a] [b] [c]
+
+public class CheckTriangleSides {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        
+        boolean output = (a >= b + c);
+
+        System.out.println(output);
+
+    }
+}
